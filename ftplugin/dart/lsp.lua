d977a4ed5b1840a5aae4dc5bb55d08717266cd3b
Brian Chen <brianchen8990@gmail.com>
2022-07-18 20:45:25 +0200

    fix: Only attach lsp once for >0.7 version

--- ftplugin/dart/lsp.lua
+++ ftplugin/dart/lsp.lua
@@ -1,5 +1,12 @@
-if vim.b.did_ftplugin_dart_lsp then
-  return
+local vim_version = vim.version()
+
+if vim_version.minor > 7 then
+      -- nightly
+   if vim.b.did_ftplugin_dart_lsp then
+     return
+   end
+   vim.b.did_ftplugin_dart_lsp = true
+else
+   require('flutter-tools.lsp').attach()
 end
-vim.b.did_ftplugin_dart_lsp = true
-require('flutter-tools.lsp').attach()
+
