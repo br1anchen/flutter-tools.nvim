b0ef0ea0b7ab96877ae777828c981771d3e11e69
Brian Chen <brianchen8990@gmail.com>
2022-07-18 20:56:43 +0200

    fix: remove only attach lsp once logic

--- ftplugin/dart/lsp.lua
+++ ftplugin/dart/lsp.lua
@@ -1,12 +1,2 @@
-local vim_version = vim.version()
-
-if vim_version.minor > 7 then
-      -- nightly
-   if vim.b.did_ftplugin_dart_lsp then
-     return
-   end
-   vim.b.did_ftplugin_dart_lsp = true
-else
-   require('flutter-tools.lsp').attach()
-end
+require('flutter-tools.lsp').attach()
 
