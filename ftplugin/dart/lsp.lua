db99020d100ba21d214c8adc3868260535bacc55
Brian Chen <brianchen8990@gmail.com>
2022-07-18 21:01:36 +0200

    Revert "fix: remove only attach lsp once logic"
    This reverts commit b0ef0ea0b7ab96877ae777828c981771d3e11e69.

--- ftplugin/dart/lsp.lua
+++ ftplugin/dart/lsp.lua
@@ -1,2 +1,12 @@
-require('flutter-tools.lsp').attach()
+local vim_version = vim.version()
+
+if vim_version.minor > 7 then
+      -- nightly
+   if vim.b.did_ftplugin_dart_lsp then
+     return
+   end
+   vim.b.did_ftplugin_dart_lsp = true
+else
+   require('flutter-tools.lsp').attach()
+end
 
